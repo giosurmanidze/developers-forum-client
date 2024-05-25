104d27aaf27aa8a9b641e1290d325470afb74062
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-05-25 13:09:56 +0400

    feat/routing

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <RouterView />
 </template>
 
---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from "../components/HelloWorld.vue";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/home',
+            name: 'home',
+            component: HelloWorld,
+          },
+    ]});
+
+export default router
